407786ea796412a641ee6a822e6af7c9bc82d3a9
hedi691 <hedi.tarchouni@gmail.com>
2012-10-02 14:22:56 +0200

    commit du prog de test + modif dans marsupial

--- src/Marsupial.java
+++ src/Marsupial.java
@@ -7,6 +7,7 @@ public abstract class Marsupial extends Animal {
 	public Marsupial(String nom) {
 		super(nom);
 		this.cSommeil = new SommeilNid();
+		this.estReveille=true;
 		// TODO Auto-generated constructor stub
 	}
 	
@@ -19,6 +20,18 @@ public abstract class Marsupial extends Animal {
 	public abstract void dormir();
 	
 	public abstract void seReveiller();
+	
+	public void changerEtat()
+	{
+		if(this.estReveille)
+		{
+			this.dormir();
+			this.estReveille=false;
+		} else {
+			this.seReveiller();
+			this.estReveille=true;
+		}
+	}
 	
 	public Boolean getEstReveille() {
 		return estReveille;
--- src/TestZoo.java
+++ src/TestZoo.java
@@ -5,7 +5,41 @@ public class TestZoo {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//on déclare les animaux
+		Koala bernard= new Koala("Bernard");
+		Koala jean= new Koala("Jean");
+		Opossum remi= new Opossum("Remi");
+		Opossum hedi= new Opossum("Hedi");
+		Mammouth patrick=new Mammouth("Patrick le mamouth");
+		
+		//on adapte le mamouth
+		MammouthAdapte patrickAdapte=new MammouthAdapte(patrick);
+		//on créer la troupe de marsupial
+		TroupeMarsupial troupe=new TroupeMarsupial("troupe");
+		
+		//on ajoute les masupiaux a la troupe
+		troupe.ajouteMarsupial(bernard);
+		troupe.ajouteMarsupial(jean);
+		troupe.ajouteMarsupial(remi);
+		troupe.ajouteMarsupial(patrickAdapte);
+		troupe.ajouteMarsupial(hedi);
+		
+		
+		System.out.println("\n\n********Test Marsupial seul********");
+		//on change l'etat du marsupial jean
+		jean.changerEtat();
+		jean.changerEtat();
+		jean.changerEtat();
+		jean.changerEtat();
+		jean.changerEtat();
+		
+		System.out.println("\n\n********Test Marsupial troupe********");
+		//on change l'etat de toute la troupe de marsupiaux
+		troupe.changerEtat();
+		System.out.println("\n");
+		troupe.changerEtat();
+
 		
 	}
 
